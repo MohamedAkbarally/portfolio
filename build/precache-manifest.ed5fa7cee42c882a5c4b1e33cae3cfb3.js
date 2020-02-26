dccf6dad3cf2dd7f8754e559c6461c281d0ff1c4
Mohamed Akbarally <mohamed.akbarally@gmail.com>
2020-02-27 04:30:26 +1100

    Intial with build folder

---
+++ build/precache-manifest.ed5fa7cee42c882a5c4b1e33cae3cfb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f455245152ce574501a4b0c64b45e7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bfe64b57c400b480466",
+    "url": "/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "484033063ebdd81cee34",
+    "url": "/static/js/2.359edecd.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.359edecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bfe64b57c400b480466",
+    "url": "/static/js/main.7104c609.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "46a684638895ad2a320cf94d432e72f3",
+    "url": "/static/media/logo.46a68463.png"
+  }
+]);
